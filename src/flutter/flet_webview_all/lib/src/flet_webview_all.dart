8406109b6e7607a64a011b27c375ec5867fa0ea3
Rehan <you@example.com>
2026-06-04 15:57:53 -0700

    fix: simplify Dart webview_all API usage - use correct Webview widget class

--- src/flutter/flet_webview_all/lib/src/flet_webview_all.dart
+++ src/flutter/flet_webview_all/lib/src/flet_webview_all.dart
@@ -2,7 +2,7 @@ import 'package:flet/flet.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_all/webview_all.dart';
 
-class FletWebviewAllControl extends StatefulWidget {
+class FletWebviewAllControl extends StatelessWidget {
   final Control control;
 
   const FletWebviewAllControl({
@@ -11,74 +11,22 @@ class FletWebviewAllControl extends StatefulWidget {
   });
 
   @override
-  State<FletWebviewAllControl> createState() => _FletWebviewAllControlState();
-}
-
-class _FletWebviewAllControlState extends State<FletWebviewAllControl> {
-  late WebviewAllController _webViewController;
-
-  @override
-  void initState() {
-    super.initState();
-    _setupWebViewController();
-  }
-
-  void _setupWebViewController() {
-    WebviewAllController.platform.setWebviewAllDebuggingEnabled(
-      widget.control.getBool("debugging_enabled", false) ?? false,
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    String? url = widget.control.getString("url");
-    String? html = widget.control.getString("html");
-    bool allowNavigation = widget.control.getBool("allow_navigation", true) ?? true;
-    bool zoomEnabled = widget.control.getBool("zoom_enabled", true) ?? true;
-    bool javascriptEnabled = widget.control.getBool("javascript_enabled", true) ?? true;
-    String? userAgent = widget.control.getString("user_agent");
+    String? url = control.getString("url");
+    String? html = control.getString("html");
+    bool zoomEnabled = control.getBool("zoom_enabled", true) ?? true;
+    bool javascriptEnabled = control.getBool("javascript_enabled", true) ?? true;
+    String? userAgent = control.getString("user_agent");
 
-    // Determine initial content
-    if (url == null && html == null) {
-      url = "about:blank";
-    }
+    // Determine initial content - use url if available, otherwise html
+    String initialContent = url ?? html ?? "about:blank";
 
-    Widget myControl = WebviewAll(
-      onWebviewCreated: (controller) {
-        _webViewController = controller;
-      },
-      initialUrl: url,
-      initialContent: html,
-      javascriptMode: javascriptEnabled
-          ? JavascriptMode.unrestricted
-          : JavascriptMode.disabled,
-      navigationDelegate: allowNavigation
-          ? null
-          : (NavigationRequest request) {
-              return NavigationDecision.prevent;
-            },
-      userAgent: userAgent ?? "",
-      gestureNavigationEnabled: true,
-      onPageStarted: (String url) {
-        debugPrint('Page started loading: $url');
-      },
-      onPageFinished: (String url) {
-        debugPrint('Page finished loading: $url');
-      },
-      onWebResourceError: (WebResourceError error) {
-        debugPrint('Error: ${error.description}');
-      },
-      onProgress: (int progress) {
-        debugPrint('Progress: $progress%');
-      },
+    Widget myControl = Webview(
+      initialUrl: initialContent,
+      userAgent: userAgent,
       zoomEnabled: zoomEnabled,
     );
 
-    return LayoutControl(control: widget.control, child: myControl);
+    return LayoutControl(control: control, child: myControl);
   }
 }
